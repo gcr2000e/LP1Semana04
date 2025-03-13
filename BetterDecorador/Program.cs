859e63fe3261dae12d56d7a056a49d123495f9a9
gcr2000e <a22409619@alunos.ulht.pt>
2025-03-13 12:08:46 +0000

    Add new version of BetterDecorador

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -12,12 +12,21 @@ namespace BetterDecorador
         /// <param name="args"></param>
         private static void Main(string[] args)
         {
-            string s = args[0];
-            char dec = Convert.ToChar(args[1]);
-            int count = int.Parse(args[2]);
-            string decString = Decor(s, dec, count);
-            Console.WriteLine(decString);
+            if (args.Length == 3)
+            {
+                string s = args[0];
+                char dec = Convert.ToChar(args[1]);
+                int count = int.Parse(args[2]);
+                string decString = Decor(s, dec, count);
+                Console.WriteLine(decString);
+            }
+            else if (args.Length != 3)
+            {
+                Console.WriteLine(Decor());
+            }
+
         }
+
         /// <summary>
         /// Decorates a string
         /// </summary>
@@ -27,9 +36,18 @@ namespace BetterDecorador
         /// <returns>The decorated string</returns>
         private static string Decor(string s, char dec, int count)
         {
-           string decString = new string(dec, count);
-
-           return decString + s + decString;
+            string decString = new string(dec, count);
+            return decString + s + decString; 
+        }
+        
+        /// <summary>
+        /// Error message in case the user don't put any args
+        /// </summary>
+        /// <returns>The error message</returns>
+        private static string Decor()
+        {
+            Decor("User did not specify args!", '=', 3);
+            return "=== User did not specify args! ===";
         }
     }
 }
